2b4c1f3c8556141dd77e9f50ef911811f217489b
Cy Brown <cy@cybrown.fr>
2014-07-20 03:14:05 +0200

    Tests for ?: associativity

--- test/operator.spec.js
+++ test/operator.spec.js
@@ -59,15 +59,33 @@ describe ('Operator', function () {
         var ast = parser.parse('1 + 3 ? 4 : 5;');
         assert.strictEqual(ast[0].type, 'ConditionalExpression');
         assert.strictEqual(ast[0].test.type, 'CallExpression');
-        assert.strictEqual(ast[0].alternate.type, 'Literal');
         assert.strictEqual(ast[0].consequent.type, 'Literal');
+        assert.strictEqual(ast[0].alternate.type, 'Literal');
     });
 
-    it ('should parse operator ? : twice', function () {
-        var ast = parser.parse('1 + 3 ? 4 : 5;');
+    it ('should parse operator ? : twice 1', function () {
+        var ast = parser.parse('a ? b ? d : e : c;');
         assert.strictEqual(ast[0].type, 'ConditionalExpression');
-        assert.strictEqual(ast[0].test.type, 'CallExpression');
-        assert.strictEqual(ast[0].alternate.type, 'Literal');
-        assert.strictEqual(ast[0].consequent.type, 'Literal');
+        assert.strictEqual(ast[0].test.type, 'Identifier');
+        assert.strictEqual(ast[0].test.name, 'a');
+        assert.strictEqual(ast[0].consequent.type, 'ConditionalExpression');
+        assert.strictEqual(ast[0].consequent.test.name, 'b');
+        assert.strictEqual(ast[0].consequent.consequent.name, 'd');
+        assert.strictEqual(ast[0].consequent.alternate.name, 'e');
+        assert.strictEqual(ast[0].alternate.type, 'Identifier');
+        assert.strictEqual(ast[0].alternate.name, 'c');
+    });
+
+    it ('should parse operator ? : twice 2', function () {
+        var ast = parser.parse('a ? b : c ? d : e;');
+        assert.strictEqual(ast[0].type, 'ConditionalExpression');
+        assert.strictEqual(ast[0].test.type, 'Identifier');
+        assert.strictEqual(ast[0].test.name, 'a');
+        assert.strictEqual(ast[0].consequent.type, 'Identifier');
+        assert.strictEqual(ast[0].consequent.name, 'b');
+        assert.strictEqual(ast[0].alternate.type, 'ConditionalExpression');
+        assert.strictEqual(ast[0].alternate.test.name, 'c');
+        assert.strictEqual(ast[0].alternate.consequent.name, 'd');
+        assert.strictEqual(ast[0].alternate.alternate.name, 'e');
     });
 });
