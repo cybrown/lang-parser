19227b091b12b93d68ba6c65ceebed7a4e7ccb73
Cy Brown <cy@cybrown.fr>
2014-07-20 21:26:35 +0200

    Test for right associative unary operators

--- test/operator.unary.spec.js
+++ test/operator.unary.spec.js
@@ -3,6 +3,13 @@ var parser = require('../lib/parserw');
 
 describe ('Operator Unary', function () {
 
+    it ('should be right associative', function () {
+        var ast = parser.parse('+-baz;');
+        assert.strictEqual(ast.body[0].expression.operator, '+');
+        assert.strictEqual(ast.body[0].expression.argument.operator, '-');
+        assert.strictEqual(ast.body[0].expression.argument.argument.name, 'baz');
+    });
+
     it ('should parse unary operator +', function () {
         var ast = parser.parse('+baz;');
         assert.strictEqual(ast.body[0].expression.operator, '+');
