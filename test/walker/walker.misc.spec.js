ab915f2887804c0ace9f74409653473f1f9152d7
Cy Brown <cy@cybrown.fr>
2014-07-24 22:51:04 +0200

    Added walker for ExpressionStatement and Program nodes

---
+++ test/walker/walker.misc.spec.js
@@ -0,0 +1,61 @@
+var Walker = require('../../lib/walker');
+var nodes = require('../../lib/nodes');
+var assert = require('assert');
+
+describe ('Walker Misc', function () {
+
+    var walker;
+
+    beforeEach(function () {
+        walker = new Walker();
+    });
+
+    it ('should walk Program', function (done) {
+        var counter = 0;
+        var node = nodes.Program([
+            nodes.ExpressionStatement(nodes.Literal(42))
+        ]);
+        walker.on('node.Program.enter', function (node) {
+            try {
+                assert.equal(counter, 0);
+                counter++;
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.on('node.ExpressionStatement.enter', function (node) {
+            try {
+                assert.equal(counter, 1);
+                counter++;
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.on('node.Literal', function (node) {
+            try {
+                assert.equal(counter, 2);
+                assert.equal(node.value, 42);
+                counter++;
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.on('node.ExpressionStatement.leave', function (node) {
+            try {
+                assert.equal(counter, 3);
+                counter++;
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.on('node.Program.leave', function (node) {
+            try {
+                assert.equal(counter, 4);
+                done();
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.walk(node);
+    });
+});
---
+++ test/walker/walker.statement.spec.js
@@ -0,0 +1,53 @@
+var Walker = require('../../lib/walker');
+var nodes = require('../../lib/nodes');
+var assert = require('assert');
+
+describe ('Walker Expressions', function () {
+
+    var walker;
+
+    beforeEach(function () {
+        walker = new Walker();
+    });
+
+    it ('should walk ExpressionStatement', function (done) {
+        var counter = 0;
+        var node = nodes.ExpressionStatement(
+            nodes.BinaryExpression('+', nodes.Literal(1), nodes.Literal(2))
+        );
+        walker.on('node.ExpressionStatement.enter', function (node) {
+            try {
+                assert.equal(counter, 0);
+                ++counter;
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.on('node.BinaryExpression.enter', function (node) {
+            try {
+                assert.equal(counter, 1);
+                assert.equal(node.operator, '+');
+                counter++;
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.on('node.BinaryExpression.leave', function (node) {
+            try {
+                assert.equal(counter, 2);
+                counter++;
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.on('node.ExpressionStatement.leave', function (node) {
+            try {
+                assert.equal(counter, 3);
+                done();
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.walk(node);
+    });
+});
