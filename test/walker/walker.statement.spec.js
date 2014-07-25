449d77be37960911ceb7a647d526e224a889cbe6
Cy Brown <cy@cyrbown.fr>
2014-07-25 13:54:03 +0200

    Added BlockStatement walker

--- test/walker/walker.statement.spec.js
+++ test/walker/walker.statement.spec.js
@@ -50,4 +50,59 @@ describe ('Walker Expressions', function () {
         });
         walker.walk(node);
     });
+
+    it ('should walk BlockStatement', function (done) {
+        var counter = 0;
+        var node = nodes.BlockStatement(
+            nodes.ExpressionStatement(
+                nodes.BinaryExpression(
+                    '+',
+                    nodes.Literal(1),
+                    nodes.Literal(2)
+                )
+            )
+        );
+        walker.on('node.BlockStatement.enter', function (node) {
+            try {
+                assert.equal(counter, 0);
+                ++counter;
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.on('node.ExpressionStatement.enter', function (node) {
+            try {
+                assert.equal(counter, 1);
+                counter++;
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.on('node.BinaryExpression.enter', function (node) {
+            try {
+                assert.equal(counter, 2);
+                assert.equal(node.operator, '+');
+                counter++;
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.on('node.ExpressionStatement.leave', function (node) {
+            try {
+                assert.equal(counter, 3);
+                counter++;
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.on('node.BlockStatement.leave', function (node) {
+            try {
+                assert.equal(counter, 4);
+                done();
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.walk(node);
+    });
 });
