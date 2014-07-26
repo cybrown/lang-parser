630fe22b23e78d5000698d427060b1e1d98ecd0f
Cy Brown <cy@cybrown.fr>
2014-07-27 01:48:57 +0200

    Walking classes

--- test/walker/walker.declaration.spec.js
+++ test/walker/walker.declaration.spec.js
@@ -1,5 +1,6 @@
 var Walker = require('../../lib/walker');
 var nodes = require('../../lib/nodes');
+var types = require('../../lib/types');
 var assert = require('assert');
 
 describe ('Walker Declarations', function () {
@@ -72,6 +73,111 @@ describe ('Walker Declarations', function () {
         walker.on('node.ConstantDeclaration.leave', function (node) {
             try {
                 assert.equal(counter, 2);
+                done();
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.walk(node);
+    });
+
+    it ('should walk empty ClassDeclaration', function (done) {
+        var counter = 0;
+        var node = nodes.ClassDeclaration('Foo', []);
+        walker.on('node.ClassDeclaration.enter', function () {
+            try {
+                assert.equal(counter, 0);
+                counter++;
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.on('node.ClassDeclaration.leave', function () {
+            try {
+                assert.equal(counter, 1);
+                done();
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.walk(node);
+    });
+
+    it ('should walk MethodParameter', function (done) {
+        var counter = 0;
+        var node = nodes.MethodParameter('x', types.PrimitiveType(32, false, false));
+        walker.on('node.MethodParameter.enter', function (node) {
+            try {
+                assert.equal(counter, 0);
+                assert.equal(node.type.$type, 'PrimitiveType');
+                counter++;
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.on('node.MethodParameter.leave', function (node) {
+            try {
+                assert.equal(counter, 1);
+                done();
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.walk(node);
+    });
+
+    it ('should walk ClassMethod', function (done) {
+        var counter = 0;
+        var node = nodes.ClassMethod(
+            'getValue',
+            types.PrimitiveType(32, false, false),
+            [nodes.MethodParameter('x', types.PrimitiveType(32, false, false))],
+            nodes.ExpressionStatement(nodes.BinaryExpression('+', null, null))
+        );
+        walker.on('node.ClassMethod.enter', function (node) {
+            try {
+                assert.equal(counter, 0);
+                assert.equal(node.$type, 'ClassMethod');
+                counter++;
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.on('node.MethodParameter.enter', function (node) {
+            try {
+                assert.equal(counter, 1);
+                counter++;
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.on('node.MethodParameter.leave', function (node) {
+            try {
+                assert.equal(counter, 2);
+                counter++;
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.on('node.ExpressionStatement.enter', function (node) {
+            try {
+                assert.equal(counter, 3);
+                counter++;
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.on('node.ExpressionStatement.leave', function (node) {
+            try {
+                assert.equal(counter, 4);
+                counter++;
+            } catch (err) {
+                done(err);
+            }
+        });
+        walker.on('node.ClassMethod.leave', function (node) {
+            try {
+                assert.equal(counter, 5);
                 done();
             } catch (err) {
                 done(err);
