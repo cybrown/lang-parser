5d245210ec797f465dfcc21070b7d353082a04f0
Cy Brown <cy@cybrown.fr>
2014-08-02 18:56:14 +0200

    calc

--- calc.js
+++ calc.js
@@ -1,92 +1,55 @@
 'use strict';
 
+var fs = require('fs');
 var Walker = require('./lib/walker');
+var Lowerer = require('./lib/lowerer');
 var parser = require('./lib/parserw');
 
-var text = process.argv[2] || "1+1;"
-
+var text = fs.readFileSync('test.txt').toString();
 var program = parser.parse(text);
+var walker = new Walker();
+
+var walker = new Walker();
+var lowerer = new Lowerer(walker);
+lowerer.process(program);
+
+var main = program.namespaces['main'].classes['Main'].methods['main'];
+
+var runner = new Walker();
+
+var builtins = {
+    '+': function (stack) {
+        var a = stack[stack.length - 1];
+        var b = stack[stack.length - 1];
+        stack.pop();
+        stack.pop();
+        stack.push(a + b);
+    },
+    print: function (stack) {
+        console.log(stack[stack.length - 1]);
+    }
+};
 
 var stack = [];
 
-var walker = new Walker();
-
-walker.on('node.UnaryExpression.leave', function (node) {
-    var a = stack[stack.length - 1];
-    stack.pop();
-    var r = 0;
-    switch (node.operator) {
-        case '+':
-            r = +a;
-            break;
-        case '-':
-            r = -a;
-            break;
-        case '!':
-            r = !a;
-            break;
-        case '~':
-            r = ~a;
-            break;
-        default:
-            throw new Error('Unknown unary operator: ' + node.operator);
+runner.setDelegate({
+    LiteralLeave: function (node) {
+        stack.push(Number(node.value));
+    },
+    IdentifierLeave: function (node) {
+        stack.push(node.name);
+    },
+    CallExpressionLeave: function (node) {
+        var method = builtins[stack[stack.length - 1]];
+        stack.pop();
+        method(stack);
+    },
+    ExpressionStatementLeave: function (node) {
+        stack.pop();
+    },
+    onUnknown: function (node) {
+        console.error('Unknown node type: ' + node.$type);
     }
-    stack.push(r);
 });
 
-walker.on('node.BinaryExpression.leave', function (node) {
-    var b = stack[stack.length - 1];
-    stack.pop();
-    var a = stack[stack.length - 1];
-    stack.pop();
-    var r = 0;
-    switch (node.operator) {
-        case '+':
-            r = a + b;
-            break;
-        case '-':
-            r = a - b;
-            break;
-        case '*':
-            r = a * b;
-            break;
-        case '/':
-            r = a / b;
-            break;
-        case '%':
-            r = a % b;
-            break;
-        case '&':
-            r = a & b;
-            break;
-        case '|':
-            r = a | b;
-            break;
-        case '^':
-            r = a ^ b;
-            break;
-        case '<<':
-            r = a << b;
-            break;
-        case '>>':
-            r = a >> b;
-            break;
-        case '||':
-            r = a || b;
-            break;
-        case '&&':
-            r = a && b;
-            break;
-        default:
-            throw new Error('Unknown binary operator: ' + node.operator);
-    }
-    stack.push(r);
-});
-
-walker.on('node.Literal', function (node) {
-    stack.push(parseInt(node.value));
-});
-
-walker.walk(program);
-
-console.log(stack[0]);
+runner.walk(main.body);
