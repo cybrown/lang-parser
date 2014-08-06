2b9de9b5eb8f3c150aebba129b857c8ed3437797
Cy Brown <cy.brown59@gmail.com>
2014-08-06 12:57:42 +0200

    Reorganized tests for typer

---
+++ test/typer/typer.internals.spec.js
@@ -0,0 +1,78 @@
+var assert = require('assert');
+var Typer = require('../../lib/typer');
+var Walker = require('../../lib/walker');
+var nodes = require('../../lib/nodes');
+
+describe ('Typer internals', function () {
+
+    var typer;
+    var boolClass;
+	var int32Class
+	var int64Class;
+	var uint32Class;
+	var uint64Class;
+	var float32Class;
+	var float64Class;
+	var stringClass;
+	var baseClass;
+	var subClass;
+
+    beforeEach(function () {
+        typer = new Typer(new Walker());
+
+        boolClass = nodes.ClassDeclaration('bool', []);
+        int32Class = nodes.ClassDeclaration('int', []);
+        int64Class = nodes.ClassDeclaration('long', []);
+        uint32Class = nodes.ClassDeclaration('uint', []);
+        uint64Class = nodes.ClassDeclaration('long', []);
+        float32Class = nodes.ClassDeclaration('long', []);
+        float64Class = nodes.ClassDeclaration('long', []);
+        stringClass = nodes.ClassDeclaration('string', []);
+
+        typer.setType('bool', boolClass);
+        typer.setType('int32', int32Class);
+        typer.setType('int64', int64Class);
+        typer.setType('uint32', uint32Class);
+        typer.setType('uint64', uint64Class);
+        typer.setType('float32', float32Class);
+        typer.setType('float64', float64Class);
+        typer.setType('string', stringClass);
+
+        baseClass = nodes.ClassDeclaration('Base', []);
+        subClass = nodes.ClassDeclaration('Sub', []);
+    });
+
+    describe ('Type compatibility', function () {
+
+        it ('should return true with 2 int32', function () {
+            assert.equal(typer.isCompatible(int32Class, int32Class), true);
+        });
+
+        it ('should return false with int32 and int64', function () {
+            assert.equal(typer.isCompatible(int32Class, int64Class), false);
+        });
+
+        xit ('should return true with a super class as second argument', function () {
+
+        });
+
+        xit ('should return false with a super class as first argument', function () {
+
+        });
+
+        xit ('should return true with an implemented interface as second argument', function () {
+
+        });
+
+        xit ('should return true with a structural compatible type as second argument', function () {
+
+        });
+    });
+
+    describe ('Method finder', function () {
+
+        xit ('should find the most suitable method in a list for a list of types', function () {
+
+        });
+    });
+});
--- test/typer/typer.spec.js
+++ test/typer/typer.spec.js
@@ -6,121 +6,94 @@ var nodes = require('../../lib/nodes');
 describe ('Typer', function () {
 
     var typer;
-
-    var boolClass = nodes.ClassDeclaration('bool', []);
-    var int32Class = nodes.ClassDeclaration('int', []);
-    var int64Class = nodes.ClassDeclaration('long', []);
-    var uint32Class = nodes.ClassDeclaration('uint', []);
-    var uint64Class = nodes.ClassDeclaration('long', []);
-    var float32Class = nodes.ClassDeclaration('long', []);
-    var float64Class = nodes.ClassDeclaration('long', []);
-    var stringClass = nodes.ClassDeclaration('string', []);
-
-    var baseClass = nodes.ClassDeclaration('Base', []);
-    var subClass = nodes.ClassDeclaration('Sub', []);
+    var boolClass;
+    var int32Class
+    var int64Class;
+    var uint32Class;
+    var uint64Class;
+    var float32Class;
+    var float64Class;
+    var stringClass;
+    var baseClass;
+    var subClass;
 
     beforeEach(function () {
         typer = new Typer(new Walker());
-    });
 
-    describe ('Typer internals', function () {
+        boolClass = nodes.ClassDeclaration('bool', []);
+        int32Class = nodes.ClassDeclaration('int', []);
+        int64Class = nodes.ClassDeclaration('long', []);
+        uint32Class = nodes.ClassDeclaration('uint', []);
+        uint64Class = nodes.ClassDeclaration('long', []);
+        float32Class = nodes.ClassDeclaration('long', []);
+        float64Class = nodes.ClassDeclaration('long', []);
+        stringClass = nodes.ClassDeclaration('string', []);
 
-        describe ('Type compatibility', function () {
+        typer.setType('bool', boolClass);
+        typer.setType('int32', int32Class);
+        typer.setType('int64', int64Class);
+        typer.setType('uint32', uint32Class);
+        typer.setType('uint64', uint64Class);
+        typer.setType('float32', float32Class);
+        typer.setType('float64', float64Class);
+        typer.setType('string', stringClass);
 
-            it ('should return true with 2 int32', function () {
-                assert.equal(typer.isCompatible(int32Class, int32Class), true);
-            });
-
-            it ('should return false with int32 and int64', function () {
-                assert.equal(typer.isCompatible(int32Class, int64Class), false);
-            });
-
-            xit ('should return true with a super class as second argument', function () {
-
-            });
-
-            xit ('should return false with a super class as first argument', function () {
-
-            });
-
-            xit ('should return true with an implemented interface as second argument', function () {
-
-            });
-
-            xit ('should return true with a structural compatible type as second argument', function () {
-
-            });
-        });
-
-        describe ('Method finder', function () {
-
-            xit ('should find the most suitable method in a list for a list of types', function () {
-
-            });
-        });
+        baseClass = nodes.ClassDeclaration('Base', []);
+        subClass = nodes.ClassDeclaration('Sub', []);
     });
 
     describe('Literal', function () {
 
         it ('should get the type of true', function () {
             var node = nodes.Literal('true');
-            typer.setType('bool', boolClass);
             typer.process(node);
             assert.equal(node.type, boolClass);
         });
 
         it ('should get the type of false', function () {
             var node = nodes.Literal('false');
-            typer.setType('bool', boolClass);
             typer.process(node);
             assert.equal(node.type, boolClass);
         });
         
         it ('should get the type of a int32', function () {
             var node = nodes.Literal('1');
-            typer.setType('int32', int32Class);
             typer.process(node);
             assert.equal(node.type, int32Class);
         });
 
         it ('should get the type of a int64', function () {
             var node = nodes.Literal('1l');
-            typer.setType('int64', int64Class);
             typer.process(node);
             assert.equal(node.type, int64Class);
         });
 
         xit ('should get the type of a uint32', function () {
             var node = nodes.Literal('1u');
-            typer.setType('uint32', uint32Class);
             typer.process(node);
             assert.equal(node.type, uint32Class);
         });
 
         xit ('should get the type of a uint64', function () {
             var node = nodes.Literal('1lu');
-            typer.setType('uint64', uint64Class);
             typer.process(node);
             assert.equal(node.type, uint64Class);
         });
 
         xit ('should get the type of a float32', function () {
             var node = nodes.Literal('1.f');
-            typer.setType('float32', float32Class);
             typer.process(node);
             assert.equal(node.type, float32Class);
         });
 
         xit ('should get the type of a float64', function () {
             var node = nodes.Literal('1.0');
-            typer.setType('float64', float64Class);
             typer.process(node);
             assert.equal(node.type, float64Class);
         });
 
         xit ('should get the type of a string', function () {
             var node = nodes.Literal('"hello world"');
-            typer.setType('string', stringClass);
             typer.process(node);
             assert.equal(node.type, stringClass);
         });
@@ -151,7 +124,6 @@ describe ('Typer', function () {
         it ('should set the type of the expression', function () {
             var nodeIdentifier = nodes.Identifier('foo');
             nodeIdentifier.type = int32Class;
-            typer.setType('int32', int32Class);
             var node = nodes.AssignmentExpression(
                 '=',
                 nodeIdentifier,
@@ -179,7 +151,6 @@ describe ('Typer', function () {
     describe('ConditionalExpression', function () {
 
         it ('should check the type of test, must be boolean', function () {
-            typer.setType('bool', boolClass);
             var node = nodes.ConditionalExpression(
                 nodes.Literal('1'),
                 nodes.Literal('2'),
